46fb8896ca8e5c9671af3dbd5e7162d01612fb6f
davigomesflorencio <davigomesflorencio@alu.ufc.br>
2025-06-02 17:01:21 -0300

    feat: Alarm Wear App - First Version

---
+++ app/src/main/java/davi/android/alarmapp/domain/model/Alarm.kt
@@ -0,0 +1,28 @@
+package davi.android.alarmapp.domain.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import davi.android.alarmapp.core.DatabaseConstants
+
+@Entity(tableName = DatabaseConstants.TABLE_ALARM)
+data class Alarm(
+    @PrimaryKey(autoGenerate = true)
+    val dbId: Long = 0,
+    @ColumnInfo(name = "vibration")
+    var vibration: Boolean,
+    @ColumnInfo(name = "repeatDays")
+    var repeatDays: String,
+    @ColumnInfo(name = "minute")
+    var min: Int,
+    @ColumnInfo(name = "hour")
+    var hour: Int,
+    @ColumnInfo(name = "disabled")
+    var disabled: Boolean,
+    @ColumnInfo(name = "soundActive")
+    var soundActive: Boolean
+) {
+    fun formattedTime(): String {
+        return hour.toString().padStart(2, '0') + ":" + min.toString().padStart(2, '0')
+    }
+}
