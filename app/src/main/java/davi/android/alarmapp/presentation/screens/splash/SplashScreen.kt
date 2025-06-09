b47523e7ccafeb82cb4f7c7f4b0b431de94f91b0
davigomesflorencio <davigomesflorencio@alu.ufc.br>
2025-06-09 18:55:42 -0300

    feat - Add splash Screen

---
+++ app/src/main/java/davi/android/alarmapp/presentation/screens/splash/SplashScreen.kt
@@ -0,0 +1,46 @@
+package davi.android.alarmapp.presentation.screens.splash
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.wear.compose.material.Scaffold
+import davi.android.alarmapp.R
+import davi.android.alarmapp.presentation.navigation.RouteAlarms
+import kotlinx.coroutines.delay
+import kotlin.time.Duration.Companion.seconds
+
+@Composable
+fun SplashScreen(backStack: SnapshotStateList<Any>) {
+    LaunchedEffect(Unit) {
+        delay(2.seconds)
+        backStack.clear()
+        backStack.add(RouteAlarms)
+    }
+
+    Scaffold(
+        Modifier.background(color = Color.Black),
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painterResource(R.drawable.ic_logo),
+                contentDescription = "",
+                modifier = Modifier.size(48.dp)
+            )
+        }
+    }
+}
