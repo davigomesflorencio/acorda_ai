60d95f41e84ac9f1f5832f53f2396e77b459b53d
davigomesflorencio <davigomesflorencio@alu.ufc.br>
2025-06-09 15:07:01 -0300

    refactor - Descomment

--- app/src/main/java/davi/android/alarmapp/presentation/screens/alarms/Alarms.kt
+++ app/src/main/java/davi/android/alarmapp/presentation/screens/alarms/Alarms.kt
@@ -1,5 +1,12 @@
 package davi.android.alarmapp.presentation.screens.alarms
 
+import android.Manifest
+import android.app.Activity
+import android.app.AlarmManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.gestures.Orientation
@@ -10,8 +17,12 @@ import androidx.compose.foundation.rememberOverscrollEffect
 import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -49,33 +60,33 @@ fun Alarms(
         }
     )
 
-//    val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+    val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
 
-//    var hasPermission by remember { mutableStateOf(alarmManager.canScheduleExactAlarms()) }
+    var hasPermission by remember { mutableStateOf(alarmManager.canScheduleExactAlarms()) }
 
-//    val permissionListener = remember {
-//        object : BroadcastReceiver() {
-//            override fun onReceive(context: Context?, intent: Intent?) {
-//                hasPermission = alarmManager.canScheduleExactAlarms()
-//            }
-//        }
-//    }
+    val permissionListener = remember {
+        object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                hasPermission = alarmManager.canScheduleExactAlarms()
+            }
+        }
+    }
 
-//    DisposableEffect(Unit) {
-//        context.registerReceiver(
-//            permissionListener,
-//            IntentFilter(AlarmManager.ACTION_SCHEDULE_EXACT_ALARM_PERMISSION_STATE_CHANGED)
-//        )
-//        onDispose {
-//            context.unregisterReceiver(permissionListener)
-//        }
-//    }
+    DisposableEffect(Unit) {
+        context.registerReceiver(
+            permissionListener,
+            IntentFilter(AlarmManager.ACTION_SCHEDULE_EXACT_ALARM_PERMISSION_STATE_CHANGED)
+        )
+        onDispose {
+            context.unregisterReceiver(permissionListener)
+        }
+    }
 
     LaunchedEffect(Unit) {
-//        val activity = context as? Activity // Or obtain activity differently
-//        if (activity?.shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS) == false) {
-//            permissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
-//        }
+        val activity = context as? Activity // Or obtain activity differently
+        if (activity?.shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS) == false) {
+            permissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+        }
     }
 
     val customSplitSwitchColors = SwitchButtonDefaults.splitSwitchButtonColors(
