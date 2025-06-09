c037311a0e1881ff2d2c9a58df447fcce4cf918b
davigomesflorencio <davigomesflorencio@alu.ufc.br>
2025-06-09 16:31:59 -0300

    build - Update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "davi.android.alarmapp"
         minSdk = 33
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.2.0"
 
         javaCompileOptions {
             annotationProcessorOptions {
